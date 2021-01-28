a8292e4df0e98083d723f06f3e658d8b609fe940
Hefesto23 <vini.ra@outlook.com>
2021-01-28 19:52:28 -0300

    chore: add express, editor config, prettier and airbnb linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,25 @@
 module.exports = {
-  extends: 'standard-with-typescript',
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json'
-  }
-}
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", {"eslint-disable-next-line": 'off'}],
+    "@typescript-eslint/interface-name-prefix": 'off',
+    "@typescript-eslint/explicit-function-return-type": 'off',
+    "@typescript-eslint/no-explicit-any": 'off',
+  },
+};
---
+++ src/main/server.ts
@@ -0,0 +1,7 @@
+import app from "./config/app";
+
+async function bootstrap() {
+  await app.listen(3000, () => console.log(`Server running in localhost:3000`));
+}
+
+bootstrap();
