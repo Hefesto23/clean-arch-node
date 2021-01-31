ee2ab480a2c8afe6c6f3caad6a77ee6efb130ddb
Hefesto23 <vini.ra@outlook.com>
2021-01-31 19:35:44 -0300

    refactor: add error model to get product usecase

--- src/data/usecases/get-product-api.usecase.ts
+++ src/data/usecases/get-product-api.usecase.ts
@@ -1,3 +1,4 @@
+import { ErrorModel } from '../../domain/models/error.model';
 import { ProductModel } from '../../domain/models/product.model';
 import { IECommerceApiService } from '../../domain/services/ecommerce-api.interface';
 import { IGetProduct } from '../../domain/usecases/get-product.interface';
@@ -9,7 +10,7 @@ export class GetProductFromApiUseCase implements IGetProduct {
     this.eCommerceApiService = eCommerceApiService;
   }
 
-  async getProductById(id: number): Promise<ProductModel> {
+  async getProductById(id: number): Promise<ProductModel | ErrorModel> {
     return this.eCommerceApiService.getProductById(id);
   }
 }
