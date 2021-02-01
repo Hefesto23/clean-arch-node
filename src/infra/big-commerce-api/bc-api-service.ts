ae0c283c6f399368983fa50785deacb80f9ed75c
Hefesto23 <vini.ra@outlook.com>
2021-02-01 13:18:01 -0300

    feat: create BigCommerce service

---
+++ src/infra/big-commerce-api/bc-api-service.ts
@@ -0,0 +1,36 @@
+import { ErrorModel } from '../../domain/models/error.model';
+import { BcErrorDto } from './dtos/bc-error.dto';
+import { ProductModel } from '../../domain/models/product.model';
+import { IECommerceApiService } from '../../domain/services/ecommerce-api.interface';
+import { ProductDto } from './dtos/product.dto';
+import { IHttpHandler } from '../../domain/services/http-handler.interface';
+import {
+  BC_HASH,
+  BC_HTTP_HEADER,
+  URL_BIG_COMMERCE,
+} from '../../config/api-variables';
+
+export class BigCommerceApiService implements IECommerceApiService {
+  private readonly httpHandler: IHttpHandler;
+
+  constructor(httpHandler: IHttpHandler) {
+    this.httpHandler = httpHandler;
+  }
+
+  async getProductById(productId: number): Promise<ProductModel | ErrorModel> {
+    return this.httpHandler
+      .get(
+        `${URL_BIG_COMMERCE}${BC_HASH}/v3/catalog/products/${productId}`,
+        BC_HTTP_HEADER,
+      )
+      .then((response) => new ProductDto(response.data))
+      .catch((error) => {
+        if (
+          error.response &&
+          (error.response.status === 400 || error.response.status === 404)
+        )
+          return new BcErrorDto(error.response);
+        throw error;
+      });
+  }
+}
